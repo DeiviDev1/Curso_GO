350a6fdd1f28686c0be632b94aa9b32069af4551
Deivison Rocha <deivison.rocha@mercadolivre.com>
2023-01-09 18:48:54 -0300

    msj

---
+++ fundamentos/primeiro/primeiro.go
@@ -0,0 +1,15 @@
+package main
+
+//Programas executaveis iniciam pelo pacote main
+import "fmt"
+
+/*
+Códigos em GO são organizados em pacotes e para usa-los é necessario declarar um ou varios imports
+*/
+func main() {
+	//Main é a porta de entra de um programa GO
+
+	fmt.Print("Primeiro ")
+	fmt.Print("Programa!")
+
+}
